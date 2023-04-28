ab7ed7fce39814731018656de399e7dd27a58343
Chris Andrews <andrewsc33@icloud.com>
2023-04-27 17:00:51 -0700

    removed twitter api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
 
+
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
 
+
   return (
     <>
+    
       <Head>
         <title>GPTweet</title>
         <meta name="description" content="GPTweet" />
@@ -14,6 +17,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex h-screen w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">GPTweeet</h1>
+            <h2 className="text-6xl font-bold">Welcome!</h2>
         <p className="mt-3 text-2xl">
           {session ? (
             <>
