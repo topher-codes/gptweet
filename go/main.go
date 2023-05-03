cab60a56928dbb1195897d680da9ac0c852dbe3a
Chris Andrews <andrewsc33@icloud.com>
2023-05-03 13:00:35 -0700

    feat
    timeline fetch
    
    we can now grab a user's timeline

--- go/main.go
+++ go/main.go
@@ -5,10 +5,15 @@ import (
     "context"
 
     "github.com/michimani/gotwi"
+    "github.com/michimani/gotwi/fields"
+    "github.com/michimani/gotwi/tweet/timeline"
+    tlt "github.com/michimani/gotwi/tweet/timeline/types"
 
     "github.com/michimani/gotwi/user/userlookup"
     "github.com/michimani/gotwi/user/userlookup/types"
 )
+
+
 
 func main() {
     c, err := gotwi.NewClient(&gotwi.NewClientInput{
@@ -32,6 +37,31 @@ func main() {
         fmt.Println("ID: ", gotwi.StringValue(u.Data.ID))
         fmt.Println("Name: ", gotwi.StringValue(u.Data.Name))
 
+    //List tweets input
+
+    input := &tlt.ListTweetsInput{
+        ID: gotwi.StringValue(u.Data.ID),
+        TweetFields: fields.TweetFieldList{
+            fields.TweetFieldCreatedAt,
+            fields.TweetFieldText,
+            fields.TweetFieldAuthorID,
+        },
+    }
+
+
+
+    p2, err := timeline.ListTweets(context.Background(), c, input)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    for _, t := range p2.Data {
+        fmt.Println("Text: ", gotwi.StringValue(t.Text))
+        fmt.Println("AuthorID: ", gotwi.StringValue(t.AuthorID))
+    }
+
+        
 
 }
 
